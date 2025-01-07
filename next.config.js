c0ddb04354e13e1440169f01983ef5af8b046d73
Duarte Vinha <d.vinha@campus.fct.unl.pt>
2025-01-07 14:47:11 +0000

    Commit package

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',
+    basePath: '/chronic-uveitis-predictor',
+    images: {
+      unoptimized: true,
+    },
+  }
+  
+  module.exports = nextConfig
