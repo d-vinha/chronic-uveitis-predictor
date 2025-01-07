c34aa1dea009777b2ab8c51f52dfe00b053c66a4
d-vinha <d.vinha@campus.fct.unl.pt>
2025-01-07 16:28:54 +0000

    commit

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    basePath: '/chronic-uveitis-predictor',
-    images: {
-      unoptimized: true,
-    },
-  }
-  
-  module.exports = nextConfig
+  images: {
+    unoptimized: true,
+  },
+}
+
+module.exports = nextConfig
